55305a16df476fc46dd842777aac98e782d5f546
Pritesh Gupta <120109061+PRITESHJGUPTA@users.noreply.github.com>
2024-03-19 09:59:16 +0530

    Add files via upload

---
+++ AreaRectangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class AreaRectangle
+{
+	public static void main(String ar[])
+	{
+	float l,b,A;
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter the length and Area :");
+	b=sc.nextFloat();
+	A=sc.nextFloat();
+
+	l=A/b;
+	System.out.println("The length is :"+l);
+	}
+}
+
+	
